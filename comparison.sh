fe57c19f86f443dcfd098664f1124374f622ffbc
Shoun1 <dasshounak2003@gmail.com>
2025-03-09 05:32:46 +0530

    A shell script to compare two numbers

---
+++ comparison.sh
@@ -0,0 +1,12 @@
+#/bin/bash
+read X
+read Y
+if [ $X -lt $Y ]; then
+    echo "X is less than Y"
+fi
+if [ $X -gt $Y ]; then
+    echo "X is greater than Y"
+fi
+if [ $X -eq $Y ]; then
+    echo "X is equal to Y"
+fi
